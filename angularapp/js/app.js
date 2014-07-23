b4f4984ac54adbc9b3faa65440a037f8177715b9
jdavidagudelo <jdaaa2009@gmail.com>
2014-07-23 17:52:30 -0500

    Página inicial

--- angularapp/js/app.js
+++ angularapp/js/app.js
@@ -30,40 +30,47 @@
 			templateUrl: "product-title.html"
 		};
 	});
-	var phones = [
-			{
-				name : 'Motorola',
-				price : 7.95,
-				description : 'Figura',
-				canPurchase : true,
-				soldOut : false,
-				images : [ "img/phones/dell-streak-7.0.jpg",
-						"img/phones/dell-venue.3.jpg" ],
-				reviews : [ {
-					stars : 5,
-					body : "buen producto",
-					author : "productowner@gmail.com"
-				}, {
-					stars : 4,
-					body : "buen producto",
-					author : "productowner@gmail.com"
-				}, ],
-
-			}, {
-				name : 'Samsung',
-				price : 4.95,
-				description : 'Figura',
-				canPurchase : true,
-				soldOut : false,
-				images : [ "img/phones/dell-streak-7.0.jpg" ],
-				reviews : [ {
-					stars : 5,
-					body : "buen producto",
-					author : "productowner@gmail.com"
-				}, {
-					stars : 4,
-					body : "buen producto",
-					author : "productowner@gmail.com"
-				}, ],
-			}, ];
+    var cliente = {
+        tipoDocumento: {id: 1, name: "Cedula"},
+        numeroDocumento: 1037524435,
+        primerNombre: "Juan",
+        segundoNombre: "David",
+        primerApellido: "Agudelo",
+        segundoApellido: "Alvarez",
+        tipoCliente: {id: 1, name: "Afiliado"},
+        fechaNacimiento: "12/03/1990"
+    };
+    var vehiculo =
+            {
+                tipoVehivulo: {id: 1, name: "Campero"},
+                referencia: "129992",
+                numeroPoliza: 1666272,
+                fechaInicioVigenciaPoliza: "12/03/2013",
+                fechaFinVigenciaPoliza: "12/03/2017"
+            };
+    var incidente = {
+        fechaIncidente: "12/03/2014",
+        motivoIncidente: "Este es el motivo",
+        descripcion: "Descripción del Incidente",
+        valorAproximadoReparacion: "10000000",
+        culpable: "SI",
+        lugarIncidente: "calle abc #123",
+        vehiculo: {
+            tipoVehivulo: {id: 1, name: "Campero"},
+            referencia: "129992",
+            numeroPoliza: 1666272,
+            fechaInicioVigenciaPoliza: "12/03/2013",
+            fechaFinVigenciaPoliza: "12/03/2017"
+        },
+        cliente: {
+            tipoDocumento: {id: 1, name: "Cedula"},
+            numeroDocumento: 1037524435,
+            primerNombre: "Juan",
+            segundoNombre: "David",
+            primerApellido: "Agudelo",
+            segundoApellido: "Alvarez",
+            tipoCliente: {id: 1, name: "Afiliado"},
+            fechaNacimiento: "12/03/1990"
+        }
+    };
 })();
