665a1671a368c9f1f9b4d20da06216bdc5caeb37
jdavidagudelo <jdaaa2009@gmail.com>
2014-07-23 18:18:14 -0500

    Las listas requeridas son obtenidas a partir de objetos JSON en el Controller, que será modificado en el futuro por servicios rest

--- angularapp/js/app.js
+++ angularapp/js/app.js
@@ -2,34 +2,46 @@
  * 
  */
 (function() {
-	var app = angular.module('autos', []);
-	app.controller("StoreController", function() {
-		this.products = phones;
-	});
-	app.controller("PanelController", function() {
-		this.tab = 1;
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		};
-		this.isSelected = function(checkTab) {
-			return this.tab === checkTab;
-		};
-	});
-	app.controller("ReviewController", function(){
-		this.review = {};
-		this.addReview = function(product)
-		{
-			this.review.createdOn = Date.now();
-			product.reviews.push(this.review);
-			this.review = {};
-		};
-	});
-	app.directive('productTitle', function(){
-		return {
-			 restrict: 'E',
-			templateUrl: "product-title.html"
-		};
-	});
+    var app = angular.module('autos', []);
+    app.controller("StoreController", function() {
+        this.products = phones;
+    });
+    app.controller("PanelController", function() {
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+    app.controller("TipoDocumentoController", function()
+    {
+        this.tiposDocumento = tiposDocumentoDummy;
+    });
+    app.controller("TipoVehiculoController", function()
+    {
+        this.tiposVehiculo = tiposVehiculoDummy;
+    });
+    app.controller("TipoAfiliacionController", function()
+    {
+        this.tiposAfiliacion = tiposAfiliacionDummy;
+    });
+    app.controller("ReviewController", function() {
+        this.review = {};
+        this.addReview = function(product)
+        {
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+    app.directive('productTitle', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-title.html"
+        };
+    });
     var cliente = {
         tipoDocumento: {id: 1, name: "Cedula"},
         numeroDocumento: 1037524435,
@@ -40,6 +52,13 @@
         tipoCliente: {id: 1, name: "Afiliado"},
         fechaNacimiento: "12/03/1990"
     };
+    var tiposDocumentoDummy = [
+        {id: 1, name: "Cedula"}, {id: 2, name: "Tarjeta de Identidad"}, {id: 3, name: "Registro Civil"}, {id: 4, name: "Pasaporte"}
+    ];
+    var tiposAfiliacionDummy = [
+        {id: 1, name: "Afiliado"}, {id: 2, name: "Asegurado"}
+    ];
+    var tiposVehiculoDummy = [{id: 1, name: "Campero"}, {id: 2, name: "Automóvil"}, {id: 3, name: "Moto"}];
     var vehiculo =
             {
                 tipoVehivulo: {id: 1, name: "Campero"},
