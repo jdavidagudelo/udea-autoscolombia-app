9749d33282854e195b596fb7821d6325c93e3164
jdavidagudelo <jdaaa2009@gmail.com>
2014-07-31 18:03:15 -0500

    Se incluyo boton para consulta de datos de clientes y vehiculos usando documentos JSON en codigo de acuerdo con lo especificado por el equipo de servicios

--- js/app.js
+++ js/app.js
@@ -19,9 +19,44 @@
         this.addIncidente = function(){
             //aqui se debe consumir el servicio para almacenar los incidentes
             incidentes.push(this.incidente);
+            this.incidente.Cliente.FechaNacimiento = new Date(this.incidente.Cliente.FechaNacimiento);
+            this.incidente.Cliente.FechaNacimiento = this.incidente.Cliente.FechaNacimiento.format("m/d/Y");
+            this.incidente.Vehiculo.FechaInicioVigenciaP = new Date(this.incidente.Vehiculo.FechaInicioVigenciaP);
+            this.incidente.Vehiculo.FechaInicioVigenciaP = this.incidente.Vehiculo.FechaInicioVigenciaP.format("m/d/Y");
+            this.incidente.Vehiculo.FechaFinVigenciaP = new Date(this.incidente.Vehiculo.FechaFinVigenciaP);
+            this.incidente.Vehiculo.FechaFinVigenciaP = this.incidente.Vehiculo.FechaFinVigenciaP.format("m/d/Y");
+            this.incidente.FechaIncidente = new Date(this.incidente.FechaIncidente);
+            this.incidente.FechaIncidente = this.incidente.FechaIncidente.format("m/d/Y");
             alert(JSON.stringify(incidentes));
             this.incidente = {};
         };
+        this.getDatosCliente = function(){
+        	this.incidente.Cliente = retornoDummy.Cliente;
+        	this.incidente.Vehiculo = retornoDummy.Vehiculo;
+        	if(this.incidente.Cliente.Tipo == "Afiliado")
+        	{
+        		this.incidente.Cliente.Tipo = 1;
+        	}
+        	else if(this.incidente.Cliente.Tipo == "Asegurado")
+        	{
+        		this.incidente.Cliente.Tipo = 2;
+        	}
+        	if(this.incidente.Vehiculo.Tipo == "Automovil")
+        	{
+        		this.incidente.Vehiculo.Tipo = 2;
+        	}
+        	if(this.incidente.Vehiculo.Tipo == "Campero")
+        	{
+        		this.incidente.Vehiculo.Tipo = 1;
+        	}
+        	if(this.incidente.Vehiculo.Tipo == "Moto")
+        	{
+        		this.incidente.Vehiculo.Tipo = 3;
+        	}
+        	this.incidente.Cliente.FechaNacimiento = new Date(retornoDummy.Cliente.FechaNacimiento);
+        	this.incidente.Vehiculo.FechaInicioVigenciaP = new Date(retornoDummy.Vehiculo.FechaInicioVigenciaP);
+        	this.incidente.Vehiculo.FechaFinVigenciaP = new Date(retornoDummy.Vehiculo.FechaFinVigenciaP);
+    	};
     });
 
 //Código para formatear el campo de valor reparación de incidente como un valor monetario en pesos
@@ -98,28 +133,28 @@ app.directive('realTimeCurrency', function ($filter, $locale) {
 
 //termina cod
 
-    app.controller("TipoDocumentoController", function()
+    app.controller("TipoDocumentoController", function($http)
     {
+    	
         //aqui se debe consumir el servicio de tipos de documento
         this.tiposDocumento = tiposDocumentoDummy;
     });
-
-    app.controller("MotivoIncidenteController", function()
+    app.controller("MotivoIncidenteController", function($http)
     {
         //aqui se debe consumir el servicio de motivos de incidente
         this.motivosIncidente = motivosIncidentesDummy;
     });
-    app.controller("TipoVehiculoController", function()
+    app.controller("TipoVehiculoController", function($http)
     {
         //aqui se debe consumir el servicio de tipo de vehiculo            
         this.tiposVehiculo = tiposVehiculoDummy;
     });
-    app.controller("TipoAfiliacionController", function()
+    app.controller("TipoAfiliacionController", function($http)
     {
         //aqui se debe consumir el servicio de tipos de clientes
         this.tiposAfiliacion = tiposAfiliacionDummy;
     });
- 
+
     //Ejemplos del JSON esperado por el FrontEnd
     var incidentes = [];
     var cliente = {
@@ -133,14 +168,91 @@ app.directive('realTimeCurrency', function ($filter, $locale) {
         fechaNacimiento: "12/03/1990"
     };
     var tiposDocumentoDummy = [
-        {id: "C", name: "Cedula"}, {id: "T", name: "Tarjeta de Identidad"}, {id: "R", name: "Registro Civil"}, {id: "P", name: "Pasaporte"}
+        {
+            "Codigo": "C",
+            "TipoDoc": "Cedula de Ciudadania"
+        },
+        {
+            "Codigo": "T",
+            "TipoDoc": "Tarjeta de Identidad"
+        },
+        {
+            "Codigo": "CE",
+            "TipoDoc": "Cedula de Extranjeria"
+        },
+        {
+            "Codigo": "P",
+            "TipoDoc": "Pasaporte"
+        },
+        {
+            "Codigo": "R",
+            "TipoDoc": "Registro Civil"
+        }
     ];
     var tiposAfiliacionDummy = [
         {id: 1, name: "Afiliado"}, {id: 2, name: "Asegurado"}
     ];
     var tiposVehiculoDummy = [{id: 1, name: "Campero"}, {id: 2, name: "Automóvil"}, {id: 3, name: "Moto"}];
-    var motivosIncidentesDummy= [{id:1, name:"Exceso de velocidad"}, {id:2, name:"Imprudencia del conductor"}, {id:3, name:"Imprudencia del peatón"}, {id:4, name:"Ebriedad del conductor"},
-    {id:5, name:"Imprudencia del pasajero"}, {id:6, name:"Exceso de carga"}, {id:7, name:"Desacato a las señales de tránsito"}, {id:8, name:"Falla mecánica"}, {id:9, name:"Falta de luces, mal estado en las vías, mala señalización y otros."}];
+    var motivosIncidentesDummy= 
+   [
+        {
+            "Codigo": "1",
+            "Texto": "Exceso de velocidad"
+        },
+        {
+            "Codigo": "2",
+            "Texto": "Imprudencia del conductor"
+        },
+        {
+            "Codigo": "3",
+            "Texto": "Imprudencia del peatón"
+        },
+        {
+            "Codigo": "4",
+            "Texto": "Ebriedad del conductor"
+        },
+        {
+            "Codigo": "5",
+            "Texto": "Imprudencia del pasajero"
+        },
+        {
+            "Codigo": "6",
+            "Texto": "Exceso de carga"
+        },
+        {
+            "Codigo": "7",
+            "Texto": "Desacato a las señales de tránsito"
+        },
+        {
+            "Codigo": "8",
+            "Texto": "Falla mecánica"
+        },
+        {
+            "Codigo": "9",
+            "Texto": "Falta de luces, mal estado de las vías, mala señalización y otros"
+        }
+    ]
+;
+var retornoDummy = {
+    "Cliente": {
+        "TipoDocumento": "C",
+        "NumeroDocumento": 1234567890,
+        "PrimerNombre": "Daniel",
+        "SegundoNombre": "Alexander",
+        "PrimerApellido": "Arango",
+        "SegundoApellido": "Arboleda",
+        "Tipo": "Afiliado",
+        "FechaNacimiento": "9/20/1991",
+        "FechaExpDoc": "20/10/2009",
+    },
+    "Vehiculo": {
+        "Tipo": "Automovil",
+        "Referencia": "Referencia",
+        "NumeroPoliza": 123456,
+        "FechaInicioVigenciaP": "01/01/2014",
+        "FechaFinVigenciaP": "01/01/2015"
+    }
+};
     var vehiculo =
             {
                 tipoVehivulo: {id: 1, name: "Campero"},
